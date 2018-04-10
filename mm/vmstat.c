5e098d47e08c6d71a6517fa08823677125c5545a
Roman Gushchin <guro@fb.com>
2018-04-10 16:27:36 -0700

    mm: introduce NR_INDIRECTLY_RECLAIMABLE_BYTES
    Patch series "indirectly reclaimable memory", v2.
    
    This patchset introduces the concept of indirectly reclaimable memory
    and applies it to fix the issue of when a big number of dentries with
    external names can significantly affect the MemAvailable value.
    
    This patch (of 3):
    
    Introduce a concept of indirectly reclaimable memory and adds the
    corresponding memory counter and /proc/vmstat item.
    
    Indirectly reclaimable memory is any sort of memory, used by the kernel
    (except of reclaimable slabs), which is actually reclaimable, i.e.  will
    be released under memory pressure.
    
    The counter is in bytes, as it's not always possible to count such
    objects in pages.  The name contains BYTES by analogy to
    NR_KERNEL_STACK_KB.
    
    Link: http://lkml.kernel.org/r/20180305133743.12746-2-guro@fb.com
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-Commit: eb59254608bc1d42c4c6afdcdce9c0d3ce02b318
    Git-Repo: git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
    Change-Id: Ie15abc33dcb13091e3acfa04dd55c664e1a24e70
    Signed-off-by: Vijayanand Jitta <vjitta@codeaurora.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -749,6 +749,7 @@ const char * const vmstat_text[] = {
 	"nr_swapcache",
 
 	/* enum writeback_stat_item counters */
+	"nr_indirectly_reclaimable",
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
